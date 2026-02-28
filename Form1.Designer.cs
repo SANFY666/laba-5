b464484fac2630f2aa0552fe4c9a3cf0bcd63fe5
Olexandr <sanfyintrom@gmail.com>
2026-02-28 23:39:29 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace laba_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.numericUpDownN = new System.Windows.Forms.NumericUpDown();
+            this.labelResultA = new System.Windows.Forms.Label();
+            this.labelResultB = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.BackColor = System.Drawing.Color.PaleGreen;
+            this.buttonCalculate.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCalculate.Location = new System.Drawing.Point(484, 420);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(150, 45);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Start";
+            this.buttonCalculate.UseVisualStyleBackColor = false;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.Location = new System.Drawing.Point(355, 224);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInput.Size = new System.Drawing.Size(239, 121);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // numericUpDownN
+            // 
+            this.numericUpDownN.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownN.Location = new System.Drawing.Point(514, 361);
+            this.numericUpDownN.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownN.Name = "numericUpDownN";
+            this.numericUpDownN.Size = new System.Drawing.Size(120, 32);
+            this.numericUpDownN.TabIndex = 2;
+            this.numericUpDownN.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelResultA
+            // 
+            this.labelResultA.AutoSize = true;
+            this.labelResultA.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResultA.Location = new System.Drawing.Point(87, 534);
+            this.labelResultA.Name = "labelResultA";
+            this.labelResultA.Size = new System.Drawing.Size(26, 23);
+            this.labelResultA.TabIndex = 3;
+            this.labelResultA.Text = "а)";
+            // 
+            // labelResultB
+            // 
+            this.labelResultB.AutoSize = true;
+            this.labelResultB.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResultB.Location = new System.Drawing.Point(85, 570);
+            this.labelResultB.Name = "labelResultB";
+            this.labelResultB.Size = new System.Drawing.Size(28, 23);
+            this.labelResultB.TabIndex = 4;
+            this.labelResultB.Text = "б)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(189, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(531, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Напиши масив рядків (кожен з нового рядка)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(287, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Вибери число n:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(91, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(708, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Salmon;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(293, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 45);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(85, 483);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "РЕЗУЛЬТАТ:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(862, 676);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelResultB);
+            this.Controls.Add(this.labelResultA);
+            this.Controls.Add(this.numericUpDownN);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonCalculate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Лабораторна робота 5 Зубко Олександр 201-ТК 9варіант";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.NumericUpDown numericUpDownN;
+        private System.Windows.Forms.Label labelResultA;
+        private System.Windows.Forms.Label labelResultB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBoxInput.Clear();
+            numericUpDownN.Value = 1;
+            labelResultA.Text = "а)";
+            labelResultB.Text = "б)";
+
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            string[] stringsArray = textBoxInput.Lines;
+
+            if (stringsArray.Length == 0)
+            {
+                MessageBox.Show("напиши щось");
+                return;
+            }
+
+            // кількість однакових рядків 
+            int duplicateCount = 0;
+            var groupedStrings = stringsArray.GroupBy(s => s);
+
+            foreach (var group in groupedStrings)
+            {
+                if (group.Count() > 1)
+                {
+                    duplicateCount += group.Count();
+                }
+            }
+            labelResultA.Text = $"Кількість однакових рядків (разом): {duplicateCount}";
+
+            // кількість рядків що починаються з n однакових символів
+            int n = (int)numericUpDownN.Value;
+            int startsWithNIdenticalCount = 0;
+
+            foreach (string str in stringsArray)
+            {
+                if (str.Length >= n)
+                {
+                    bool isIdentical = true;
+                    char firstChar = str[0];
+
+                    // перевірка чи перші n символів збігаються з першим
+                    for (int i = 1; i < n; i++)
+                    {
+                        if (str[i] != firstChar)
+                        {
+                            isIdentical = false;
+                            break;
+                        }
+                    }
+
+                    if (isIdentical)
+                    {
+                        startsWithNIdenticalCount++;
+                    }
+                }
+            }
+            labelResultB.Text = $"Рядків, що починаються з {n} однакових символів: {startsWithNIdenticalCount}";
+        }
+    }
+}
